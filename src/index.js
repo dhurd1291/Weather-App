10192fad3f4b453284891f83ae59a11145e31995
dhurd1291 <danielle.r.hurd@gmail.com>
2022-06-09 17:41:41 -0400

    working on forecast

--- src/index.js
+++ src/index.js
@@ -136,7 +136,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
-
+getForecast(response.data.coord);
+function getForecast(coordinates) {
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+  getForecast(response.data.coord);
+}
 
 
 displayForecast();
